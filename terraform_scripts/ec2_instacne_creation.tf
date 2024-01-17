0ddb027bcff0e0710032f6c9054c0d145e96be00
raavangokul007 <raavangokuldd@gmail.com>
2024-01-18 02:08:27 +0530

    first commit

---
+++ terraform_scripts/ec2_instacne_creation.tf
@@ -0,0 +1,13 @@
+provider "aws"{
+region = "us-east-2"
+}
+
+resource "aws_instance" "AWSEC2Instance"{
+ami = "ami-0cd3c7f72edd5b06d"
+instance_type = "t2.micro"
+key_name = "devopskey"
+security_groups = ["launch-wizard-2"]
+tags={
+Name = "tomcatappserver"
+}
+}
